c0c92927fa2560f034d43a2f99b8d2a25fd67b86
torchhound <unknownmasayoshi@gmail.com>
2018-01-19 19:39:06 -0500

    Realized S3 was unnecessary for posts, only need it for images

--- endpoints/createThread.js
+++ endpoints/createThread.js
@@ -1,4 +1,3 @@
-import uuid from "uuid";
 import moment from "moment";
 import AWS from "aws-sdk";
 import * as dynamoTools from "../tools/dynamoTools";
@@ -11,34 +10,20 @@ const s3 = new AWS.S3({ region: config.region });
 
 export function main(event, context, callback) {
     const data = JSON.parse(event.body);
-    var initialPostOut = JSON.parse(data.posts[0]);
-    const initialPostId = uuid.v4();
-    initialPostOut.postId = initialPostId;
-    const initialPostIn = JSON.stringify(initialPostOut)
 
-    var putParams = {
-        Bucket: config.postBucket,
-        Key: initialPostId,
-        Body: initialPostIn
-    }
-    var putObjectPromise = s3.putObject(putParams).promise();
-    putObjectPromise.then(function(data) {
-        const dbParams = {
-            TableName: config.tableName,
-            Thread: {
-                threadId: moment().valueOf(),
-                threadTitle: data.threadTitle,
-                posts: [initialPostId]
-            }
-        };
+    const dbParams = {
+        TableName: config.tableName,
+        Thread: {
+            threadId: moment().valueOf(),
+            threadTitle: data.threadTitle,
+            posts: data.posts
+        }
+    };
 
-        var putDbPromise = dynamoTools.call("put", dbParams);
-        putDbPromise.then(function(data) {
-            callback(null, success(params.Item));
-        }).catch(function(err) {
-            callback(null, failure({ status: false }));
-        })
+    var putDbPromise = dynamoTools.call("put", dbParams);
+    putDbPromise.then(function(data) {
+        callback(null, success(params.Item));
     }).catch(function(err) {
         callback(null, failure({ status: false }));
-    });
+    })
 }
